697dbaafa51317b9e1bf12e07d18578a6544640d
Ronijs Barenitis <ronijs.barenitis@gmail.com>
2023-10-10 18:42:07 +0200

    Create and write java file

---
+++ Casting1.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String args[]) {
+      double x=10.7;
+      double y=25.4;
+      double z=x+y;
+
+      System.out.println("Sum of x+y = " + (int)z);
+    }
+}
