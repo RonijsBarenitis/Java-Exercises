1152379f5094abf0f8c3a92b1fdf40102bf96fe7
Ronijs Barenitis <ronijs.barenitis@gmail.com>
2023-10-11 11:47:14 +0200

    Create the intervalControl function

--- LogicalOperators1.java
+++ LogicalOperators1.java
@@ -1,1 +1,17 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int low = 10;
+      int upp = 20;
+      int control = 22;
+      
+      System.out.println("Lower number is "+low);
+      System.out.println("Upper number is "+upp);
+      System.out.println("Is the control number between lower and higher number? " + intervalControl(low,upp,control));
+    }
+    
+     public static boolean intervalControl(int a, int b, int c){
+         boolean d = c >= a && c <= 20;
+         return d;
+     }
+}
 
