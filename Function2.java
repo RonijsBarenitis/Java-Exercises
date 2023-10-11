ab6b7eb5dcd5623d3d761237867e85f5cc089015
Ronijs Barenitis <ronijs.barenitis@gmail.com>
2023-10-11 09:19:22 +0200

    Create the function lengthOf

--- Function2.java
+++ Function2.java
@@ -1,1 +1,12 @@
+public class MyClass {
+    public static void main(String args[]) {
+      String myString = "This is a string 36 characters long.";
 
+      System.out.println("Length of myString = " + lengthOf(myString));
+    }
+    
+     public static int lengthOf(String a){
+         int x = a.length();
+         return x;
+     }
+}
