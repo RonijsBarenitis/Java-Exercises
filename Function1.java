6497acfa83eeaa994923851941530d4fb9440fd5
Ronijs Barenitis <ronijs.barenitis@gmail.com>
2023-10-11 09:06:37 +0200

    Create the function sumOf

--- Function1.java
+++ Function1.java
@@ -1,1 +1,14 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int x=10;
+      int y=25;
 
+      System.out.println("Sum of x+y = " + sumOf(x,y));
+      System.out.println("Sum of 99+15 = " + sumOf(99,15));
+    }
+    
+     public static int sumOf(int a, int b){
+         int c=a+b;
+         return c;
+     }
+}
