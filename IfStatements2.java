8119e4a9996af2d511158e8be1b27486cef94fdc
Ronijs Barenitis <ronijs.barenitis@gmail.com>
2023-10-11 16:11:21 +0200

    Write the programm to check string length

--- IfStatements2.java
+++ IfStatements2.java
@@ -1,1 +1,14 @@
-
+public class MyClass {
+    public static void main(String args[]) {
+      String myString = "aaaaaaaaaaa";
+      if(myString.length()>10){
+          System.out.println("Lunghezza maggiore di 10");
+      }
+      else if(myString.length()<10){
+          System.out.println("Lunghezza minore di 10");
+      }
+      else if(myString.length()==10){
+          System.out.println("Lunghezza pari a 10");
+      }
+    }
+}
