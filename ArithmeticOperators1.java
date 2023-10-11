a4896f5e69f6a6ce86cda1d6be2ce246b92e0769
Ronijs Barenitis <ronijs.barenitis@gmail.com>
2023-10-11 09:38:15 +0200

    Create the quotient function

--- ArithmeticOperators1.java
+++ ArithmeticOperators1.java
@@ -1,1 +1,13 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int x = 100;
+      int y = 5;
 
+      System.out.println("Quotient of x/y = " + quotient(x,y));
+    }
+    
+     public static int quotient(int a, int b){
+         int c = a/b;
+         return c;
+     }
+}
