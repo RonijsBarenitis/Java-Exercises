43a80fad91618b9688ba5d063d6b9fc34a60636c
Ronijs Barenitis <ronijs.barenitis@gmail.com>
2023-10-11 12:06:18 +0200

    Create the isPair function

--- LogicalOperators2.java
+++ LogicalOperators2.java
@@ -1,1 +1,12 @@
-
+public class MyClass {
+    public static void main(String args[]) {
+      int num = 100;
+      
+      System.out.println("Is the given number a pair number? " + isPair(num));
+    }
+    
+     public static boolean isPair(int a){
+         boolean b = a%2==0;
+         return b;
+     }
+}
