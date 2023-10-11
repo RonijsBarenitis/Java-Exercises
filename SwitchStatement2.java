a97042bca8016b5d5ce7d35ee808a455ee3d4d87
Ronijs Barenitis <ronijs.barenitis@gmail.com>
2023-10-11 16:41:36 +0200

    Create the isAlgebricOp function

--- SwitchStatement2.java
+++ SwitchStatement2.java
@@ -1,1 +1,28 @@
-
+public class MyClass {
+    public static void main(String args[]) {
+      char a='&';
+      System.out.println(isAlgebricOp(a));
+    }
+    
+    public static String isAlgebricOp(char x){
+        String operator="";
+        switch(x){
+            case '+':
+                operator = "Addition";
+                break;
+            case '-':
+                operator = "Subtraction";
+                break;
+            case '*':
+                operator = "Multiplication";
+                break;
+            case '/':
+                operator = "Division";
+                break;
+            default:
+                operator = "Error, not an elgebric operator";
+                break;
+        }
+        return operator;
+    }
+}
