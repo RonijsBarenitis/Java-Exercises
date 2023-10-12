0c831b48990b7f50eee023605afff23f55afda8d
Ronijs Barenitis <ronijs.barenitis@gmail.com>
2023-10-12 17:28:59 +0200

    Rename inverseMatrix to swapmatrix, and complete the function

--- MultidimensionalArray2.java
+++ MultidimensionalArray2.java
@@ -2,26 +2,34 @@ public class MyClass {
     public static void main(String args[]) {
       
       int[][] matrix = {{1,2,3},{5,6,7}};
-      inverseMatrix(matrix);
-
-    }
-    
-    public static void inverseMatrix(int[][] a){
-        int[][] newMatrix = {{},{}};
-        
-        System.out.println(a.length);
-        for(int i = 0; i < a[i].length; i++){
-            for(int j = 0; j < a.length; j++){
-                newMatrix[i][j] = a[j][i];
-            }
-        }
-        
-        for(int i = 0; i < newMatrix.length; i++){
-            for(int j = 0; j < newMatrix[i].length; j++){
-                System.out.print(newMatrix[i][j]);
+      
+      System.out.println("The original matrix\n");
+      for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                System.out.print(matrix[i][j]+ " ");
             }
             System.out.println();
         }
         
+      System.out.println("\nThe  matrix after swapping rows with columns\n");
+      swapMatrix(matrix);
+
+    }
+    
+    public static void swapMatrix(int[][] a){
+        
+        int newMatrix[][] = new int[3][2];
+        
+        for(int i = 0; i < a.length; i++){
+            for(int j = 0; j < a[i].length; j++){
+                newMatrix[j][i] = a[i][j];
+            }
+        }
+        for(int i = 0; i < newMatrix.length; i++){
+            for(int j = 0; j < newMatrix[i].length; j++){
+                System.out.print(newMatrix[i][j]+ " ");
+            }
+            System.out.println();
+        }
     }
 }
