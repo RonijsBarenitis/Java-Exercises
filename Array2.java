c28c1015dd91f3e60168f24e08b55a9da0cfdf19
Ronijs Barenitis <ronijs.barenitis@gmail.com>
2023-10-12 10:06:02 +0200

    Create the charArr function

--- Array2.java
+++ Array2.java
@@ -1,1 +1,23 @@
-
+public class MyClass {
+    public static void main(String args[]) {
+      
+      System.out.println("All occurrences of the character 'a' will be printed bellow from the given array. In case there are none, it will return 0");
+      charArr();
+      
+    }
+    
+    public static void charArr(){
+        
+        char[] array = {'a','b','c','d','e','a'};
+        boolean hasA = false;
+        for(int i=0; i < array.length; i++){
+            if(array[i]=='a'){
+                System.out.print(array[i]+" ");
+                hasA = true;
+            }
+        }
+        if(hasA==false){
+            System.out.print("0");
+        }
+    }
+}
