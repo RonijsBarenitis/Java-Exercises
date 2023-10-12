1af529ca8edddff949861ec971cc3bd94d8f0ff5
Ronijs Barenitis <ronijs.barenitis@gmail.com>
2023-10-12 07:58:15 +0200

    Create the numSum function

--- WhileLoop.java
+++ WhileLoop.java
@@ -1,1 +1,17 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int num=5;
 
+      System.out.println("The sum of all number from 1 to 'num' is " + numSum(num));
+    }
+    
+    public static int numSum(int x){
+        int ctrl=1;
+        int sum=0;
+        while(ctrl<x){
+            sum+=ctrl;
+            ctrl++;
+        }
+        return sum;
+    }
+}
