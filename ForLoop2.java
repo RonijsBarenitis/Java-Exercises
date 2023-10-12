8ca0fe53657941d45e12cf1f5c186ce29c2280a4
Ronijs Barenitis <ronijs.barenitis@gmail.com>
2023-10-12 08:59:02 +0200

    Create the inverseNumList function

--- ForLoop2.java
+++ ForLoop2.java
@@ -1,1 +1,17 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int numOrigin=100;
+      int numOps=5;
 
+      System.out.println("Natural numbers in inverse order from 'numOrigin' printed 'numOps' times");
+      inverseNumList(numOrigin, numOps);
+    }
+    
+    public static void inverseNumList(int x, int y){
+        
+        for(int i=y; i>0; i--){
+            System.out.print(x+" ");
+            x--;
+        }
+    }
+}
