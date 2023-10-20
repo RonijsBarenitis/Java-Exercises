54799eac4b220ee90973a2aa65837e9ccc05a181
Ronijs Barenitis <ronijs.barenitis@gmail.com>
2023-10-20 08:17:05 +0200

    Create getters and setters for Student class

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,18 @@ public class Main {
 
         Student student1 = new Student("Mario", "Rossi", 12345);
 
-        System.out.println("Student 1 data - "+student1);
+        System.out.println("Nome dello studente: "+student1.getName());
+        System.out.println("Cognome dello studente: "+student1.getSurname());
+        System.out.println("Numero identificativo dello studente: "+student1.getIdNumber());
+
+        student1.setName("Marco");
+        student1.setSurname("Fortebraccio");
+        student1.setIdNumber(54321);
+
+        System.out.println("\nNome aggiornato dello studente: "+student1.getName());
+        System.out.println("Cognome aggiornato dello studente: "+student1.getSurname());
+        System.out.println("Numero identificativo aggiornato dello studente: "+student1.getIdNumber());
+
+
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,14 +1,38 @@
 public class Student {
 
-    private final String name;
+    public String name;
 
-    private final String surname;
+    public String surname;
 
-    private final int idNumber;
+    public int idNumber;
 
     public Student(String name, String surname, int idNumber){
         this.name = name;
         this.surname = surname;
+        this.idNumber = idNumber;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getSurname(){
+        return surname;
+    }
+
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+
+    public int getIdNumber(){
+        return idNumber;
+    }
+
+    public void setIdNumber(int idNumber){
         this.idNumber = idNumber;
     }
 
