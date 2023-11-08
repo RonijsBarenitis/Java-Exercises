85eff695a8c78119a15aa1cc671f7aad2cd4f2fb
Ronijs Barenitis <ronijs.barenitis@gmail.com>
2023-11-08 15:31:14 +0100

    Exercise complete

---
+++ src/Coordinates.java
@@ -0,0 +1,2 @@
+public record Coordinates(int x, int y) {
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Coordinates coordinates= new Coordinates(5,4);
+
+        System.out.println("The coordinates are: " +coordinates);
+    }
+}
