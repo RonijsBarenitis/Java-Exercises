41bceab3cc06b130b87893dbf8601aa97593c506
Ronijs Barenitis <ronijs.barenitis@gmail.com>
2024-01-18 09:50:04 +0100

    Exercise complete

---
+++ src/main/java/develhope/exercise1/CiaoController.java
@@ -0,0 +1,18 @@
+package develhope.exercise1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+
+public class CiaoController {
+
+    @GetMapping(path = "/ciao")
+    public String ciao(@RequestParam String nome, @RequestParam String provincia){
+        return "Ciao " +nome +", com'è il tempo in " +provincia +"?";
+    }
+
+}
