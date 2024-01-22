9cda79712426bb84250dcf94d4dc452df874c1d6
Ronijs Barenitis <ronijs.barenitis@gmail.com>
2024-01-22 17:50:07 +0100

    Exercise complete

---
+++ src/main/java/com/develhope/Controller/_con/GET/e/POST/Esercizio3Application.java
@@ -0,0 +1,13 @@
+package com.develhope.Controller._con.GET.e.POST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercizio3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esercizio3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/develhope/Controller/_con/GET/e/POST/Esercizio3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develhope.Controller.con.GET.e.POST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esercizio3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
