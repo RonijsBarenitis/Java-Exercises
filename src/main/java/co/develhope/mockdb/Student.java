c837d34b2f1095ef096ec010d87cd616516cfbbd
Ronijs Barenitis <ronijs.barenitis@gmail.com>
2024-02-01 10:19:10 +0100

    Exercise complete

---
+++ src/main/java/co/develhope/mockdb/Student.java
@@ -0,0 +1,21 @@
+package co.develhope.mockdb;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+}
