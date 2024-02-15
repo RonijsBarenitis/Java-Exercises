12c029569e0db8b2f9fe88a6c735be64a3d176c6
Ronijs Barenitis <ronijs.barenitis@gmail.com>
2024-02-15 17:19:32 +0100

    Exercise cpmplete

---
+++ src/main/java/co/develhope/SpringInterceptor2/entities/Month.java
@@ -0,0 +1,21 @@
+package co.develhope.SpringInterceptor2.entities;
+
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Month {
+
+
+    private int monthNumber;
+
+    private String englishName;
+
+    private String italianName;
+
+    private String germanName;
+
+}
